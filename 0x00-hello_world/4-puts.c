d63309ef9a281ee0edc4b4e30931a0c502fbe49d
xAbu042 <abumapesa.ak@gmail.com>
2022-03-10 17:19:42 +0300

    Fix error in 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *main - prints the phrase "Programming is like building a multilingual puzzle"
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
