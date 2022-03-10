1c9eb70e1519f7190e4adb035c29b7c0837cda46
xAbu042 <abumapesa.ak@gmail.com>
2022-03-10 17:11:11 +0300

    Script that prints using the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Return: 0 if executed properly, non-zero otherwise
+ *main - prints the phrase "Programming is like building a multilingual puzzle"
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
