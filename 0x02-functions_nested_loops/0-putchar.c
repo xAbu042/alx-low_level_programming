2c57d6ed158d99315c5116425962bc1af642d012
xAbu042 <abumapesa.ak@gmail.com>
2022-03-15 14:32:33 +0300

    Created a new directory and a file that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints _putchar
+ * followed by a new line
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
