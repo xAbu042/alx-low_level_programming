2d8026c93ffa8331aa6208a264ac20dee8118537
xAbu042 <abumapesa.ak@gmail.com>
2022-03-17 14:45:03 +0300

    Checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: alphabet to check
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a continuation of the C functions and nested loops exercises in ALX SWE
