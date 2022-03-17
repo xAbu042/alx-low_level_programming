7c439c68aa977917d693cb989c0125dd6f1c9d1e
xAbu042 <abumapesa.ak@gmail.com>
2022-03-17 17:25:40 +0300

    Fix the stubborn error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, 0 to 14
+ * more_numbers - print 10 times the numbers, 0 to 14
  * followed by a new line
  */
 void more_numbers(void)
