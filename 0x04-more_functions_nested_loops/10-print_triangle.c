3e5a077355029f453bd2f408c79844a0fee69ce7
xAbu042 <abumapesa.ak@gmail.com>
2022-03-18 00:01:17 +0300

    Hot fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,13 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 1; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j < i; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j < size; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
