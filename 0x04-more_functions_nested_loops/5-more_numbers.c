7cadd2f315549050691752ca5cf4327e5b07f65c
xAbu042 <abumapesa.ak@gmail.com>
2022-03-17 16:56:58 +0300

    Fix error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	j = '0';
+	j = 0;
 
-	while (j <= '9')
+	while (j <= 9)
 	{
-		for (i = 0; i >= 14; i++)
+		for (i = 0; i = 15; i++)
 		{
 			if (i >= 10)
 			putchar(i / 10 + '0');
