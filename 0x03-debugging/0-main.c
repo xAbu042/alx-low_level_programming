d60d08ad00604187ccc75820be4270d3de3664b6
xAbu042 <abumapesa.ak@gmail.com>
2022-03-18 18:42:35 +0300

    Q1 commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - main
  * Return: 0
  */
 int main(void)
@@ -9,7 +9,9 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
+
 }
