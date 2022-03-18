b54e4838c459c99bd0890a8d29270a4e3fd4d283
xAbu042 <abumapesa.ak@gmail.com>
2022-03-18 15:26:52 +0300

    Comment out bad code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,19 +4,19 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
-/*	int i; */
+	int i; 
 
 	printf("Infinite loop incoming :(\n");
+
+	i = 0;
 /**
- *	i = 0;
- *
+ * No increase in variable causing infinite loop 
  *	while (i < 10)
  *	{
  *		putchar(i);
- *       }
+ *      }
  */
 	printf("Infinite loop avoided! \\o/\n");
 
