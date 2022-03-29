2c633ac03a38c5ed37cad92d00a0a4f1f563b60c
xAbu042 <abumapesa.ak@gmail.com>
2022-03-29 17:14:03 +0300

    Two qn commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be checked
+ * @needle: substring to check in the string above
+ * Return: Pointer to the beginning of the located
+ * substring, or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; *haystack != '\0'; i++)
+	{
+		for (j = 0; *needle != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
