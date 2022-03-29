30e35ea6855008d8b3c293b98cb4f5419ddf1194
xAbu042 <abumapesa.ak@gmail.com>
2022-03-29 18:12:39 +0300

    Fixing error

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; *haystack != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; *needle != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
-			{
 				break;
-			}
 		}
 		if (!needle[j])
 			return (&haystack[i]);
