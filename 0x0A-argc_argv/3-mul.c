c4329b9745da84ebc9877bd0daad5eba9f061707
xAbu042 <abumapesa.ak@gmail.com>
2022-04-02 14:50:21 +0300

    Multiply 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: string converted to integer
+ */
+int _atoi(char *s)
+{
+	int i, j, k, l, len, digit;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	len = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && l == 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (j % 2)
+				digit = -digit;
+			k = k * 10 + digit;
+			l = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			l = 0;
+		}
+		i++;
+	}
+
+	if (l == 0)
+		return (0);
+
+	return (k);
+}
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 if program does not receive two argc
+ * 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, result;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = _atoi(argv[1]);
+	j = _atoi(argv[2]);
+
+	result = i * j;
+
+	printf("%d\n", result);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,4 +2,8 @@ This project introduces us to C Arguments to main and the purpose is to familiar
 
 File 0-whatsmyname.c - Program that prints its name, followed by a new line.
 
+File 1-args.c - Program that prints the number of arguments passed into it.
 
+File 2-args.c - Program that prints all arguments it receives.
+
+File 3-mul.c - Program that multiplies two numbers
