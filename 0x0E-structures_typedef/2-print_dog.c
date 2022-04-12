a0cea497912bb6be8829ad150bdcdd432855abca
xAbu042 <abumapesa.ak@gmail.com>
2022-04-12 18:13:23 +0300

    Hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,10 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("(nil)");
+		d->name = ("(nil)");
 
 	if (d->owner == NULL)
-		printf("(nil)");
+		d->name = ("(nil)");
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
