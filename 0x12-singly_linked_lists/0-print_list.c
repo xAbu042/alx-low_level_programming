468b115ed06b1c38c2adf85594bcaf50b148e324
xAbu042 <abumapesa.ak@gmail.com>
2022-04-22 17:22:53 +0300

    Fix on Q0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -7,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t len = 0;
 
 	while (h)
 	{
@@ -17,9 +18,9 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
-		n++;
+		len++;
 	}
 
-	return (n);
+	return (len);
 
 }
