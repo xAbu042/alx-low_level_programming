083df4d0e9417f9aa8fe285fcd0afb9e1ff00bf9
xAbu042 <abumapesa.ak@gmail.com>
2022-05-03 20:55:38 +0300

    Error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, t, s = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
@@ -28,6 +28,6 @@ int create_file(const char *filename, char *text_content)
 
 	close(fd);
 
-	return (-1);
+	return (1);
 
 }
